3fcf5cdfe800238fb944a64f5fd268e921726f1b
im ali <chjczj@126.com>
2025-02-23 21:18:33 +0800

    feat: 更新全局配置，调整地图中心点及基础URL，优化布局样式，添加地图拾取功能

--- src/pages/scene-manage/components/map-pick.tsx
+++ src/pages/scene-manage/components/map-pick.tsx
@@ -1,9 +1,67 @@
 import MapContent from "@/components/map-content";
+import { useRef } from "react";
 
 const MapPick = () => {
+  const mapPick = useRef<any>();
+  const overlaysId = useRef<string>();
+
+  const addCustomOverlay = (lngLat) => {
+    if (!mapPick.current) return;
+
+    if (overlaysId.current) {
+      mapPick.current.removeCustomOverlayById({
+        id: overlaysId.current,
+      });
+      overlaysId.current = null;
+    }
+    let DOM = document.createElement("div");
+    DOM.className = "addByIdClass";
+    DOM.innerHTML = "";
+    // DOM.style.width = "200px";
+    // DOM.style.height = "80px";
+    let imgSrc = document.createElement("img");
+    imgSrc.src = require("@/assets/img/icon-skg-poi.png");
+    let SPAN = document.createElement("span");
+    SPAN.innerHTML = lngLat.join();
+    DOM.appendChild(imgSrc);
+    DOM.appendChild(SPAN);
+    mapPick.current.addCustomOverlay({
+      point: lngLat,
+      element: DOM,
+      offset: [0, -6],
+      customOverlaysType: "addById",
+      ended: (res) => {
+        try {
+          overlaysId.current = res.data;
+        } catch (error) {}
+      },
+    });
+  };
+
+  const onClickEvent = (res) => {
+    // this.setState({
+    //   pickupCoordinate: `${res.lngLat.lng.toFixed(6)},${res.lngLat.lat.toFixed(
+    //     6
+    //   )}`,
+    // });
+    addCustomOverlay([res.lngLat.lng.toFixed(6), res.lngLat.lat.toFixed(6)]);
+  };
+
+  const onLoad = ({ map }) => {
+    map.addTools({
+      compass: true,
+      zoomControl: true,
+    });
+    map.addEventOnMap({
+      event: "click",
+      handler: onClickEvent,
+    });
+    mapPick.current = map;
+  };
+
   return (
-    <div className="w-[100vw] h-[100vh]">
-      <MapContent></MapContent>
+    <div className="w-[calc(100vw-40px)] h-[calc(100vh-170px)]">
+      <MapContent onLoad={onLoad}></MapContent>
     </div>
   );
 };
--- src/pages/scene-manage/components/scene-form.tsx
+++ src/pages/scene-manage/components/scene-form.tsx
@@ -19,7 +19,7 @@ import { getSceneServiceList, getSceneTypes, ISceneInfo } from "../webapi";
 import MapPick from "./map-pick";
 
 const SceneForm = ({ form }: { form: FormInstance<ISceneInfo> }) => {
-  const formRef = useRef();
+  const formWrapperRef = useRef();
   const [visible, setVisible] = useState(false);
   const [file, setFile] = useState<UploadItem>();
   const [values, setValues] = useState<ISceneInfo>();
@@ -64,168 +64,168 @@ const SceneForm = ({ form }: { form: FormInstance<ISceneInfo> }) => {
   }, []);
 
   return (
-    <Form.Provider onFormSubmit={onSubmit} onFormValuesChange={onValuesChange}>
-      <Form
-        ref={formRef}
-        form={form}
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 18 }}
+    <div ref={formWrapperRef}>
+      <Form.Provider
+        onFormSubmit={onSubmit}
+        onFormValuesChange={onValuesChange}
       >
-        <Form.Item
-          label="场景名称"
-          field="sceneName"
-          rules={[{ required: true, message: "请输入场景名称" }]}
-        >
-          <Input placeholder="请输入场景名称" allowClear />
-        </Form.Item>
-        <Form.Item
-          label="场景类型"
-          field="sceneType"
-          rules={[{ required: true, message: "请选择场景类型" }]}
-        >
-          <Select placeholder="请选择场景类型">
-            {sceneTypes.map((item) => (
-              <Select.Option key={item.code} value={item.code}>
-                {item.name}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="场景服务"
-          field="sceneServiceCode"
-          rules={[{ required: true, message: "请选择场景服务" }]}
-        >
-          <Select
-            placeholder="请选择场景服务"
-            onChange={(value, option) => {
-              console.log(option);
-              // const sceneServiceName = option.label;
-              // setValues({ ...values, sceneServiceName });
-            }}
+        <Form form={form} labelCol={{ span: 6 }} wrapperCol={{ span: 18 }}>
+          <Form.Item
+            label="场景名称"
+            field="sceneName"
+            rules={[{ required: true, message: "请输入场景名称" }]}
           >
-            {sceneServiceList.map((item) => (
-              <Select.Option key={item.serviceCode} value={item.serviceCode}>
-                {item.serviceName}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="场景中心点"
-          field="sceneCenter"
-          rules={[{ required: true, message: "请输入经纬度" }]}
-        >
-          <Input.Search
-            searchButton={
-              <>
-                <Icon
-                  type="anbao-pick-up"
-                  className="text-[20px] align-[-4px] mr-[4px]"
-                />
-                拾取
-              </>
-            }
-            onSearch={() => setVisible(true)}
-            placeholder="请输入经纬度"
-            allowClear
-          />
-          <Modal
-            visible={visible}
-            title="拾取经纬度"
-            onCancel={() => setVisible(false)}
-            onConfirm={() => setVisible(false)}
-            getPopupContainer={() => formRef.current}
-            style={{
-              position: "fixed",
-              inset: 0,
-              borderRadius: 0,
-              width: "100%",
-              padding: 0,
-            }}
-            wrapStyle={{
-              padding: 0,
-              height: "100vh",
-              overflow: "auto",
-            }}
+            <Input placeholder="请输入场景名称" allowClear />
+          </Form.Item>
+          <Form.Item
+            label="场景类型"
+            field="sceneType"
+            rules={[{ required: true, message: "请选择场景类型" }]}
           >
-            <MapPick />
-          </Modal>
-        </Form.Item>
-        <Form.Item
-          label="所属行政区"
-          field="regionId"
-          rules={[{ required: true, message: "请选择所属行政区" }]}
-          normalize={(value) => value.join(",")}
-          formatter={(value) => value && value.split(",")}
-        >
-          <Cascader
-            showSearch={true}
-            placeholder="请选择行政区"
-            allowClear={true}
-            options={activityStore.areaTree}
-            fieldNames={{
-              label: "name",
-              value: "id",
-            }}
-            onChange={(value, options) => {
-              const regionName = options.map((item) => item.name).join();
-              setValues({ ...values, regionName });
-            }}
-          />
-        </Form.Item>
-        <Form.Item label="场景面积" field="sceneArea">
-          <Input
-            placeholder="请输入场景面积"
-            className="w-[calc(100%-32px)] mr-[8px]"
-            allowClear
-          />
-          m²
-        </Form.Item>
-        <Form.Item label="场景图片" field="sceneThumbnail">
-          <Upload
-            fileList={file ? [file] : []}
-            listType="picture-card"
-            accept=".jpg,.jpeg,.png"
-            showUploadList={false}
-            className={style["scene-upload"]}
-            onChange={(_, currentFile) => {
-              setFile({
-                ...currentFile,
-                url: URL.createObjectURL(currentFile.originFile),
-              });
-            }}
+            <Select placeholder="请选择场景类型">
+              {sceneTypes.map((item) => (
+                <Select.Option key={item.code} value={item.code}>
+                  {item.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="场景服务"
+            field="sceneServiceCode"
+            rules={[{ required: true, message: "请选择场景服务" }]}
           >
-            {file?.url ? (
-              <div className={style["arco-upload-list-item-picture"]}>
-                <img src={file.url} />
-                <div className="arco-upload-list-item-picture-mask leading-[88px]">
-                  <IconEdit />
+            <Select
+              placeholder="请选择场景服务"
+              onChange={(value, option) => {
+                console.log(option);
+                // const sceneServiceName = option.label;
+                // setValues({ ...values, sceneServiceName });
+              }}
+            >
+              {sceneServiceList.map((item) => (
+                <Select.Option key={item.serviceCode} value={item.serviceCode}>
+                  {item.serviceName}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="场景中心点"
+            field="sceneCenter"
+            rules={[{ required: true, message: "请输入经纬度" }]}
+          >
+            <Input.Search
+              searchButton={
+                <>
+                  <Icon
+                    type="anbao-pick-up"
+                    className="text-[20px] align-[-4px] mr-[4px]"
+                  />
+                  拾取
+                </>
+              }
+              onSearch={() => setVisible(true)}
+              placeholder="请输入经纬度"
+              allowClear
+            />
+            <Modal
+              visible={visible}
+              title="拾取经纬度"
+              onCancel={() => setVisible(false)}
+              onConfirm={() => setVisible(false)}
+              getPopupContainer={() => formWrapperRef.current}
+              style={{
+                position: "fixed",
+                inset: 0,
+                borderRadius: 0,
+                width: "100%",
+                padding: 0,
+              }}
+              wrapStyle={{
+                padding: 0,
+                height: "100vh",
+                overflow: "auto",
+              }}
+            >
+              <MapPick />
+            </Modal>
+          </Form.Item>
+          <Form.Item
+            label="所属行政区"
+            field="regionId"
+            rules={[{ required: true, message: "请选择所属行政区" }]}
+            normalize={(value) => value.join(",")}
+            formatter={(value) => value && value.split(",")}
+          >
+            <Cascader
+              showSearch={true}
+              placeholder="请选择行政区"
+              allowClear={true}
+              options={activityStore.areaTree}
+              fieldNames={{
+                label: "name",
+                value: "id",
+              }}
+              onChange={(value, options) => {
+                const regionName = options.map((item) => item.name).join();
+                setValues({ ...values, regionName });
+              }}
+            />
+          </Form.Item>
+          <Form.Item label="场景面积" field="sceneArea">
+            <Input
+              placeholder="请输入场景面积"
+              className="w-[calc(100%-32px)] mr-[8px]"
+              allowClear
+            />
+            m²
+          </Form.Item>
+          <Form.Item label="场景图片" field="sceneThumbnail">
+            <Upload
+              fileList={file ? [file] : []}
+              listType="picture-card"
+              accept=".jpg,.jpeg,.png"
+              showUploadList={false}
+              className={style["scene-upload"]}
+              onChange={(_, currentFile) => {
+                setFile({
+                  ...currentFile,
+                  url: URL.createObjectURL(currentFile.originFile),
+                });
+              }}
+            >
+              {file?.url ? (
+                <div className={style["arco-upload-list-item-picture"]}>
+                  <img src={file.url} />
+                  <div className="arco-upload-list-item-picture-mask leading-[88px]">
+                    <IconEdit />
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <div className={style["arco-upload-trigger-picture"]}>
-                <div className={style["arco-upload-trigger-picture-text"]}>
-                  <Icon type="anbao-newly-added" />
-                  <div className="mt-[2px] font-[600]">上传</div>
+              ) : (
+                <div className={style["arco-upload-trigger-picture"]}>
+                  <div className={style["arco-upload-trigger-picture-text"]}>
+                    <Icon type="anbao-newly-added" />
+                    <div className="mt-[2px] font-[600]">上传</div>
+                  </div>
                 </div>
-              </div>
-            )}
-          </Upload>
-          <div className={style["upload-desc"]}>
-            支持上传JPG/JPEG/PNG格式文件，文件大小不超过10M
-          </div>
-        </Form.Item>
-        <Form.Item label="场景说明" field="remark">
-          <Input.TextArea
-            placeholder="请输入场景说明"
-            maxLength={200}
-            autoSize={{ minRows: 4 }}
-            allowClear
-          />
-        </Form.Item>
-      </Form>
-    </Form.Provider>
+              )}
+            </Upload>
+            <div className={style["upload-desc"]}>
+              支持上传JPG/JPEG/PNG格式文件，文件大小不超过10M
+            </div>
+          </Form.Item>
+          <Form.Item label="场景说明" field="remark">
+            <Input.TextArea
+              placeholder="请输入场景说明"
+              maxLength={200}
+              autoSize={{ minRows: 4 }}
+              allowClear
+            />
+          </Form.Item>
+        </Form>
+      </Form.Provider>
+    </div>
   );
 };
 
